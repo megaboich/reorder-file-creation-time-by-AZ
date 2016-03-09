bc9468ba3e5c5c6befca28adc779ca6436efe3b9
mega.boich@gmail.com <mega.boich@gmail.com>
2016-03-09 17:48:51 +0100

    Implemented logic

--- reorder-file-creation-time-by-AZ.ps1
+++ reorder-file-creation-time-by-AZ.ps1
@@ -1,17 +1,29 @@
 param([String] $scanPath)
-#Script to read all files in directory recursively and update their creation and modification time according to their alphabet order
+#Script to read all files in directory recursively and update their creation time according to their alphabet order
 
 Write-Host "Files orderer v0.1"
 Write-Host "Working folder: $scanPath"
 
-Function ExecuteMain($scanPath){
-    $files = Get-ChildItem $scanPath | Where-Object {!$_.PSIsContainer} 
-    foreach($file in $files)
-    {
-        Write-Host $file.Fullname
+Function ScanDir($scanPath){
+    $fileNames = @(Get-ChildItem $scanPath | Where-Object {!$_.PSIsContainer} | Foreach-Object {$_.FullName} | Sort-Object)
+    $creationTimes = @(Get-ChildItem $scanPath | Where-Object {!$_.PSIsContainer} | Foreach-Object {$_.CreationTime} | Sort-Object)
+    Write-Host "Path $scanPath"
+    Write-Host "Files $($fileNames.Length)"
+    Write-Host "Dates $($creationTimes.Length)"
+    
+    for($i = 0; $i -lt $fileNames.Length; $i++){
+        $fn = $fileNames[$i]
+        $ct = $creationTimes[$i]
+        $fd = Get-Item $fn
+        Write-Host "-- $($fd.Name) --     $($fd.CreationTime) => $ct"
+    }
+    
+    $subDirectories = Get-ChildItem $scanPath | Where-Object {$_.PSIsContainer}
+    foreach($dir in $subDirectories){
+        ScanDir $dir.FullName
     }
 }
 
-ExecuteMain($scanPath)
+ScanDir($scanPath)
 
 cmd /c pause | out-null
